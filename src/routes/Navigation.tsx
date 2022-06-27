1cf30a8e922c7a6fbb9345a68e3d8b8ac198affa
juan.acuna <juan.acuna@unosquare.com>
2022-06-27 15:32:54 -0500

    Lazyload básico listo

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { BrowserRouter, Navigate } from "react-router-dom";
 import { Routes, Route, NavLink } from "react-router-dom";
 
@@ -8,27 +9,29 @@ import { routes } from "./routes";
 
 const Navigation = () => {
   return (
-    <BrowserRouter>
-      <div className="main-layout">
-        <nav>
-          <img src={logo} alt="React logo" />
-          <ul>
-            {routes.map(({ to, name }) => (
-              <li key={to}>
-                <NavLink to={to}>{name}</NavLink>
-              </li>
-            ))}
-          </ul>
-        </nav>
+    <Suspense fallback={<span>Loading...</span>}>
+      <BrowserRouter>
+        <div className="main-layout">
+          <nav>
+            <img src={logo} alt="React logo" />
+            <ul>
+              {routes.map(({ to, name }) => (
+                <li key={to}>
+                  <NavLink to={to}>{name}</NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
 
-        <Routes>
-          {routes.map(({ path, Component }) => (
-            <Route path={path} element={<Component />} key={path} />
-          ))}
-          <Route path="/*" element={<Navigate to="/lazy1" replace />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+          <Routes>
+            {routes.map(({ path, Component }) => (
+              <Route path={path} element={<Component />} key={path} />
+            ))}
+            <Route path="/*" element={<Navigate to={routes[0].to} replace />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Suspense>
   );
 };
 
