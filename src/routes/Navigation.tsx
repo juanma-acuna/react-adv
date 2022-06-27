b8d58b871bc7c44b3792eff9485a0c2f8b8f072a
juan.acuna <juan.acuna@unosquare.com>
2022-06-27 15:11:57 -0500

    Modificado Navigation para usar rutas dinamicas

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Navigate } from "react-router-dom";
 import { Routes, Route, NavLink } from "react-router-dom";
 
-import { LazyPage1, LazyPage2, LazyPage3 } from "./../01-lazyload/pages";
-
 import logo from "../logo.svg";
+// import { LazyPage1, LazyPage2, LazyPage3 } from "./../01-lazyload/pages";
+
+import { routes } from "./routes";
 
 const Navigation = () => {
   return (
@@ -12,37 +13,18 @@ const Navigation = () => {
         <nav>
           <img src={logo} alt="React logo" />
           <ul>
-            <li>
-              <NavLink
-                to="/lazy1"
-                className={({ isActive }) => (isActive ? "nav-active" : "")}
-              >
-                Lazy 1
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/lazy2"
-                className={({ isActive }) => (isActive ? "nav-active" : "")}
-              >
-                Lazy 2
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/lazy3"
-                className={({ isActive }) => (isActive ? "nav-active" : "")}
-              >
-                Lazy 3
-              </NavLink>
-            </li>
+            {routes.map(({ to, name }) => (
+              <li key={to}>
+                <NavLink to={to}>{name}</NavLink>
+              </li>
+            ))}
           </ul>
         </nav>
 
         <Routes>
-          <Route path="/lazy1" element={<LazyPage1 />} />
-          <Route path="/lazy2" element={<LazyPage2 />} />
-          <Route path="/lazy3" element={<LazyPage3 />} />
+          {routes.map(({ path, Component }) => (
+            <Route path={path} element={<Component />} key={path} />
+          ))}
           <Route path="/*" element={<Navigate to="/lazy1" replace />} />
         </Routes>
       </div>
